dbafee7383f9832efd283f8378a3e992fb868852
Ryan Wang <i@ryanc.cc>
2025-07-23 12:44:43 +0800

    Add route
    Signed-off-by: Ryan Wang <i@ryanc.cc>

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -1,9 +1,31 @@
 import { VLoading } from '@halo-dev/components'
 import { definePlugin } from '@halo-dev/console-shared'
 import 'uno.css'
-import { defineAsyncComponent } from 'vue'
+import { defineAsyncComponent, markRaw } from 'vue'
+import SimpleIconsMeilisearch from '~icons/simple-icons/meilisearch?color=#FF5CAA'
 
 export default definePlugin({
+  routes: [
+    {
+      parentName: 'ToolsRoot',
+      route: {
+        path: 'meilisearch-overview',
+        name: 'MeilisearchOverview',
+        redirect: '/plugins/meilisearch?tab=overview',
+        meta: {
+          title: 'Meilisearch 数据概览',
+          description: '查看 Meilisearch 搜索引擎的索引数据',
+          searchable: true,
+          permissions: [],
+          menu: {
+            name: 'Meilisearch 数据概览',
+            icon: markRaw(SimpleIconsMeilisearch),
+            priority: 0,
+          },
+        },
+      },
+    },
+  ],
   extensionPoints: {
     'plugin:self:tabs:create': () => {
       return [
